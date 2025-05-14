976ca823b94f46b93fc898c45c45f049a04c586b
sakshi <sakshidubey4467@gmail.com>
2025-05-14 14:45:06 +0530

    publishing it on github

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+    base: '/portfolio/',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js', 
   },
+  build: {
+  outDir: 'dist', 
+}
 });
